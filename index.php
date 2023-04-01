9b8f377d785b041db17b79b0015db4a926c2978e
Nightfly <nightfly@email.ua>
2023-04-01 14:41:56 +0300

    looks like it works

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
 
+/**
+ * Config section
+ * 
+ * OpenStreetMap: https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+ * Visicom: https://tms{s}.visicom.ua/2.0.0/planet3/base/{z}/{x}/{y}.png?key=YOUR_API_KEY
+ */
+$remoteTileServer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+/**
+ * End of config section
+ */
 define('LIBS_PATH', 'libs/');
 
 require_once (LIBS_PATH . 'api.ubrouting.php');
 require_once (LIBS_PATH . 'api.omaeurl.php');
 require_once (LIBS_PATH . 'api.kominaritile.php');
 
-$cache = new KominariTile();
+$cache = new KominariTile($remoteTileServer);
 if (ubRouting::checkGet($cache::ROUTE_TILE)) {
     $cache->renderTile(ubRouting::get($cache::ROUTE_TILE, 'vf'));
 }
--- libs/api.kominaritile.php
+++ libs/api.kominaritile.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Tile caching server implementation
+ * 
+ * OSM tile usage policy: https://operations.osmfoundation.org/policies/tiles/
+ */
 class KominariTile {
 
     /**
@@ -7,7 +12,21 @@ class KominariTile {
      *
      * @var int
      */
-    protected $cacheTime = 60;
+    protected $cacheTime = 604800; // 7 days by default
+
+    /**
+     * User agent for remote tiles fetching
+     *
+     * @var string
+     */
+    protected $userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0';
+
+    /**
+     * Contains current instance remote tile server URL template
+     *
+     * @var string
+     */
+    protected $remoteTileTemplate = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
     /**
      * Default caching directory 
@@ -32,10 +51,21 @@ class KominariTile {
     const OFFSET_X = 2;
     const OFFSET_Y = 3;
 
-    // https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+    public function __construct($template) {
+        if (!empty($template)) {
+            $this->setTemplate($template);
+        }
+    }
 
-    public function __construct() {
-        // what?
+    /**
+     * Sets current instance template
+     * 
+     * @param string $template remote tile server URL template with {s},{z},{x},{y} macro
+     * 
+     * @return void
+     */
+    protected function setTemplate($template) {
+        $this->remoteTileTemplate = $template;
     }
 
     /**
@@ -48,7 +78,116 @@ class KominariTile {
     protected function getTileCachePath($tileParts) {
         $result = '';
         if (isset($tileParts[self::OFFSET_Y])) {
-            $result = self::CACHE_PATH . $tileParts[self::OFFSET_S] . '/' . $tileParts[self::OFFSET_Z] . '/' . $tileParts[self::OFFSET_X] . '/' . $tileParts[self::OFFSET_Y];
+            $result = self::CACHE_PATH . $tileParts[self::OFFSET_S] . '/' . $tileParts[self::OFFSET_Z] . '/' . $tileParts[self::OFFSET_X] . '_' . $tileParts[self::OFFSET_Y];
+        }
+        return($result);
+    }
+
+    /**
+     * Allocates cache storage path
+     * 
+     * @param int $s
+     * @param int $z
+     * @param int $x
+     * @param string $y
+     * 
+     * @throws Exception
+     */
+    protected function allocateStorage($s, $z, $x, $y) {
+        $sPath = self::CACHE_PATH . $s;
+        $zPath = self::CACHE_PATH . $s . '/' . $z;
+        if (is_writable(self::CACHE_PATH)) {
+            if (!file_exists($sPath)) {
+                //creating sPath
+                mkdir($sPath, 0777);
+                chmod($sPath, 0777);
+                //and zPath inside
+                mkdir($zPath, 0777);
+                chmod($zPath, 0777);
+            } else {
+                //just check for zPath
+                if (!file_exists($zPath)) {
+                    mkdir($zPath, 0777);
+                    chmod($zPath, 0777);
+                }
+            }
+        } else {
+            throw new Exception('EX_CACHE_NOT_WRITABLE');
+        }
+    }
+
+    /**
+     * Receives tile from remote tileserver
+     * 
+     * @param int $s
+     * @param int $z
+     * @param int $x
+     * @param string $y
+     * 
+     * @return string
+     */
+    protected function getRemoteTile($s, $z, $x, $y) {
+        $result = '';
+        $fullImageUrl = $this->remoteTileTemplate;
+        $fullImageUrl = str_replace('{s}', $s, $fullImageUrl);
+        $fullImageUrl = str_replace('{z}', $z, $fullImageUrl);
+        $fullImageUrl = str_replace('{x}', $x, $fullImageUrl);
+        $fullImageUrl = str_replace('{y}', $y, $fullImageUrl);
+        $remoteTileServer = new OmaeUrl($fullImageUrl);
+        $remoteTileServer->setUserAgent('Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0');
+        $receivedTile = $remoteTileServer->response();
+        $error = $remoteTileServer->error();
+        $httpCode = $remoteTileServer->httpCode();
+        if (!$error AND $httpCode == 200) {
+            $result = $receivedTile;
+        }
+        return($result);
+    }
+
+    /**
+     * Returns full cached tile path
+     * 
+     * @param array $tileParts
+     * 
+     * @return string/void
+     */
+    protected function getCachedTile($tileParts) {
+        $result = '';
+        $expectedTilePath = $this->getTileCachePath($tileParts);
+        if ($expectedTilePath) {
+            //already cached?
+            $cacheTime = time() - $this->cacheTime;
+            $updateCache = false;
+            if (file_exists($expectedTilePath)) {
+                $updateCache = false;
+                if ((filemtime($expectedTilePath) > $cacheTime)) {
+                    $updateCache = false;
+                } else {
+                    $updateCache = true;
+                }
+            } else {
+                $updateCache = true;
+            }
+
+            //cache updating here
+            if ($updateCache) {
+                $s = $tileParts[self::OFFSET_S];
+                $z = $tileParts[self::OFFSET_Z];
+                $x = $tileParts[self::OFFSET_X];
+                $y = $tileParts[self::OFFSET_Y];
+                $this->allocateStorage($s, $z, $x, $y);
+                $remoteTile = $this->getRemoteTile($s, $z, $x, $y);
+                //received something?
+                if ($remoteTile) {
+                    //updating cache
+                    file_put_contents($expectedTilePath, $remoteTile);
+                    //returning full path
+                    $result = $expectedTilePath;
+                }
+            } else {
+                //cache is valid, return its path
+                $result = $expectedTilePath;
+            }
         }
         return($result);
     }
@@ -65,17 +204,15 @@ class KominariTile {
         if (!empty($tileId)) {
             $tileParts = explode(self::ROUTE_DELIMITER, $tileId);
             if (sizeof($tileParts) == 4) {
-                $expectedCachePath = $this->getTileCachePath($tileParts);
-                if (!empty($expectedCachePath)) {
-                    if (file_exists($tileFullPath)) {
-                        // get from cache
-                    } else {
-                        // put to cache
-                    }
+                $cachedTilePath = $this->getCachedTile($tileParts);
+                //already cached?
+                if ($cachedTilePath) {
+                    $tileFullPath = $cachedTilePath;
                 }
             }
         }
 
+        //rendering tile
         $tileContent = file_get_contents($tileFullPath);
         print($tileContent);
         die();
